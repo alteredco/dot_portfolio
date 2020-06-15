0bf2ba5b1495b3e673066639649aa868588ea983
Wendy <alteredco@gmail.com>
2020-06-15 12:41:08 +0100

    add more project number assets and begin to switch to sass styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import DrawerToggleBtn from '../DrawerToggleBtn/DrawerToggleBtn';
-import './Header.css';
+import './Header.scss';
 
 const Header =(props)=> {
   return (
